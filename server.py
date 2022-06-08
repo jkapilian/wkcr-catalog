563e3b48c2bc450881410a26c5515b09083ebf8f
Joshua Kapilian <kapgamesandapps2@gmail.com>
2022-06-07 23:14:37 -0700

    Add search routing

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')
 
+@app.route('/search/<query>')
+def search(query):
+   return render_template('search.html', query=query)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
--- static/main.js
+++ static/main.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
+	$("#search").keydown(e => {
+		if (e.which == 13) {
+			search();
+		}
+	});
+	$("#search-button").click(() => search());
 });
+
+function search() {
+	let query = $("#search").val().trim();
+	if (query) {
+		window.location.href = "/search/" + query;
+	}
+}
