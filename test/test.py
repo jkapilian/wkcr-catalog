e462256473092d35b3536f9b94b2bbd3f9656fcb
Joshua Kapilian <kapgamesandapps2@gmail.com>
2023-01-26 21:04:36 -0500

    Print out error codes on tests

--- test/test.py
+++ test/test.py
@@ -2,10 +2,12 @@ import requests
 
 # test that homepage loads
 r = requests.get('http://localhost')
+print(f'homepage {r.status_code}')
 assert r.status_code == 200
 
 # test basic search functionality
 r = requests.get('http://localhost/search/coltrane')
 assert r.status_code == 200
+print(f'search {r.status_code}')
 
 print('---TESTS PASSED---')
