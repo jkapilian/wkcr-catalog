49f3ebb6b7b0a32eac88637fae8e3ac0273ebc7e
Joshua Kapilian <kapgamesandapps2@gmail.com>
2023-01-26 21:06:50 -0500

    Add in sleep to test

--- test/test.py
+++ test/test.py
@@ -1,13 +1,16 @@
 import requests
+import time
 
 # test that homepage loads
 r = requests.get('http://localhost')
-print(f'homepage {r.status_code}')
 assert r.status_code == 200
+
+# give time to retrieve some data
+time.sleep(10)
 
 # test basic search functionality
 r = requests.get('http://localhost/search/coltrane')
-assert r.status_code == 200
 print(f'search {r.status_code}')
+assert r.status_code == 200
 
 print('---TESTS PASSED---')
