de04bb7e8be8ca81864c15e3be7a500ef4b0bc02
Joshua Kapilian <kapgamesandapps2@gmail.com>
2023-07-16 21:46:47 -0700

    Fix tests for nginx https redirect

--- test/test.py
+++ test/test.py
@@ -1,11 +1,11 @@
 import requests
 
 # test that homepage loads
-r = requests.get('http://localhost')
+r = requests.get('http://localhost:8000')
 assert r.status_code == 200
 
 # test basic search functionality
-r = requests.get('http://localhost/search/coltrane')
+r = requests.get('http://localhost/search/coltrane:8000')
 assert r.status_code == 200
 
 print('---TESTS PASSED---')
