cf21180081bc869237a8c02861dc6b2f647621c5
Joshua Kapilian <kapgamesandapps2@gmail.com>
2023-07-16 21:48:36 -0700

    Fix remaining tests for nginx https redirect

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ r = requests.get('http://localhost:8000')
 assert r.status_code == 200
 
 # test basic search functionality
-r = requests.get('http://localhost/search/coltrane:8000')
+r = requests.get('http://localhost:8000/search/coltrane')
 assert r.status_code == 200
 
 print('---TESTS PASSED---')
